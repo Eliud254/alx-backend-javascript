512476a47283f51e42b786cdfac90e604139ed8b
eliud <eliudobure@gmail.com>
2024-04-22 21:02:18 +0300

    ES6 javascript

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trustOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+  task2 = false;
+
+  }
+
+  return [task, task2];
+}
