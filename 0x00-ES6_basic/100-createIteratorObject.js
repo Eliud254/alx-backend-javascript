384b652f77abe019e38555f4f195f01029016cdc
eliud <eliudobure@gmail.com>
2024-04-22 23:15:09 +0300

    correction done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,3 @@
 export default function createIteratorObject(report) {
-  // Generator function to iterate through every employee in every department
-  function* iterator() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
-    }
-  }
-  
-  // Return the iterator object
-  return iterator();
+  return Object.values(report.allEmployees).flat();
 }
