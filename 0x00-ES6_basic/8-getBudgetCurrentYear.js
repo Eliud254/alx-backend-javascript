c64330372494e14bbb06468d100a9b51d4aaae78
eliud <eliudobure@gmail.com>
2024-04-22 21:47:41 +0300

    Task 8 done

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,14 +1,22 @@
+// Define a function to retrieve the current year
 function getCurrentYear() {
+  // Create a new Date object to get the current date and time
   const date = new Date();
+
+  // Return the current year using the getFullYear method
   return date.getFullYear();
 }
 
+// Define a function to get the budget for the current year
 export default function getBudgetForCurrentYear(income, gdp, capita) {
+  // Create a budget object with keys representing income, GDP, and capita for the current year
   const budget = {
     [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita
+    [`gdp-${getCurrentYear()}`]: gdp,   
+    [`capita-${getCurrentYear()}`]: capita,
   };
 
+  // Return the budget object
   return budget;
 }
+
