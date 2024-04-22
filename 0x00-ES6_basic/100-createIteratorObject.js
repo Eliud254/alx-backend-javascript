0d80ae4c74d5d2859e880a75aff0b75f03be60f2
eliud <eliudobure@gmail.com>
2024-04-22 23:19:12 +0300

    done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,3 @@
-// Create an iterator object to iterate through all employees in every department
 export default function createIteratorObject(report) {
   return Object.values(report.allEmployees).flat();
 }
