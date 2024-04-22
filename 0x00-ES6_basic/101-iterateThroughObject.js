749748570983f9bec2c08422d367dafa6f9b9ce6
eliud <eliudobure@gmail.com>
2024-04-22 23:13:06 +0300

    Correction done

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,4 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeString = '';
-
-  for (const department in reportWithIterator.allEmployees) {
-    const employees = reportWithIterator.allEmployees[department];
-    for (const employee of employees) {
-      employeeString += `${employee} | `;
-    }
-  }
-
-  // Remove the trailing ' | ' from the concatenated string
-  return employeeString.slice(0, -3);
+  // Join the iterator values using '|' as separator
+  return [...reportWithIterator].join(' | ');
 }
