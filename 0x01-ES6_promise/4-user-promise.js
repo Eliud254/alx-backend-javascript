e98a1c40d3382d8b4656b50d13df0c26ab62743c
eliud <eliudobure@gmail.com>
2024-04-27 00:10:40 +0300

    Another commit

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,8 @@
+import signUpUser from "./4-user-promise";
+
+signUpUser("Bob", "Dylan")
+  .then(user => console.log(user));
+
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
     firstName,
