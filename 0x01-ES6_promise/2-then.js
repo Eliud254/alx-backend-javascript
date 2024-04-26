ce8ac9eb39cd893ec2e388224d62cd764f43c6ef
eliud <eliudobure@gmail.com>
2024-04-26 23:29:19 +0300

    Another commit

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API'); 
+      return { status: 200, body: 'success' }; 
+    })
+    .catch(() => {
+      console.log('Got a response from the API'); 
+      return new Error(); 
+    });
+}
