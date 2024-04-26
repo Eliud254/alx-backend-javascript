a7e4bd686ad87e430b30cc133eaeaddad52fbbf0
eliud <eliudobure@gmail.com>
2024-04-26 23:03:55 +0300

    ES6 Promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Eslint-disable-next-line and no-constant-condition
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
