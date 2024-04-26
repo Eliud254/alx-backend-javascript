a47bc0c96b39d657f234528e9a15281c8b7ebb9c
eliud <eliudobure@gmail.com>
2024-04-26 23:50:03 +0300

    all done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,12 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  // Use Promise.all to collectively resolve all promises
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      // Destructure the results array to access the resolved values
-      const [photo, user] = results;
-      // Log the firstName and lastName to the console
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      const [photo, user] = result;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
-      // Log an error message if any of the promises reject
       console.log('Signup system offline');
     });
 }
-
-export default handleProfileSignup;
