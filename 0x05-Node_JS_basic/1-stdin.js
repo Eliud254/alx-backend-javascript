c6c4a63c4699473370438d525399d95f8a583b49
Eliud254 <eliudobure@gmail.com>
2024-07-21 18:44:26 +0000

    All done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.trim()}\n`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit(0);
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit(0);
 });
