2f8b46f5bbb725b701fb3fdbac0dc36d530612c4
Eliud254 <eliudobure@gmail.com>
2024-07-21 18:41:39 +0000

    All done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.trim()}\n`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit(0);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit(0);
+});
